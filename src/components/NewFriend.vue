f1774ebb38d4a233a6d5c3f65e1e9cc395c90862
Andrea Trajkovic <trajkovicandrea@gmail.com>
2023-08-09 18:31:39 +0200

    Update

---
+++ src/components/NewFriend.vue
@@ -0,0 +1,30 @@
+<template>
+  <form>
+    <div>
+      <label>Name</label>
+      <input type="text" v-model="enteredName" />
+    </div>
+    <div>
+      <label>Phone</label>
+      <input type="tel" v-model="enteredPhone" />
+    </div>
+    <div>
+      <label>E-Mail</label>
+      <input type="email" v-model="enteredEmail" />
+    </div>
+    <button>Add Contact</button>
+  </form>
+</template>
+
+<script>
+export default {
+  emits: ['add-contact'],
+  data() {
+    return {
+      enteredName: '',
+      enteredPhone: '',
+      enteredEmail: '',
+    };
+  },
+};
+</script>
